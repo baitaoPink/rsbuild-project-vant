b3a534990454be878dc5f57e28e9f1334738591e
bai tao <yixiang374@gmail.com>
2025-02-10 12:05:53 +0800

    upd: test

--- src/views/about.vue
+++ src/views/about.vue
@@ -6,8 +6,7 @@
   </template>
 
   <script setup lang="ts">
-  import { useI18n } from 'vue-i18n',
-
+  import { useI18n } from 'vue-i18n'; // Import the useI18n function from vue-i18n
 
 
   const { t } = useI18n(); // Composition API method to access translation functions
